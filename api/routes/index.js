53996e6e416981d1ef88cdccb29cdd691c86f593
isahmet089 <isahmet089@gmail.com>
2025-01-26 04:45:46 +0300

    ENV

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const config = require('../config');
 
+const { configDotenv } = require('dotenv');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' , configDotenv });
 });
 
 module.exports = router;
