98aa4a882354b08546756dadeb0e9c663defed76
isahmet089 <isahmet089@gmail.com>
2025-01-28 04:40:42 +0300

    category post.save && enum && err

--- api/lib/Response.js
+++ api/lib/Response.js
@@ -1,17 +1,26 @@
+const Enum = require('../config/Enum');
+const CustomError = require('./Error');
 class Response{
     constructor(){}
     static successResponse(data,code=200){
         return {
             data,
-            code
+            code,
         }
     }
     static errorResponse(error){
+        if(error instanceof CustomError){
+            return{
+                code:error.code,
+                message:error.message,
+                description:error.description
+            }
+        }
         return{
-            success: false,
+            code:Enum.HTTP_CODES.INTERNAL_SERVER_ERROR,
             error: {
-                message: error.message || 'Bir hata oluştu',
-                code: error.code || 500
+                message:'unknown error',
+                description:error.message  
             }
         }
     }
--- api/routes/categories.js
+++ api/routes/categories.js
@@ -2,14 +2,37 @@ const express = require('express');
 var router = express.Router();
 const Categories = require('../db/models/Categories');
 const Response = require('../lib/Response');
-
+const CustomError = require('../lib/Error');
+const Enum = require('../config/Enum');
 router.get('/', async (req, res, next) => {
     try {
         let categories = await Categories.find();
         res.json(Response.successResponse(categories));
     } catch (error) {
         // Hata detaylarını ekleyelim
-        res.json(Response.errorResponse(error));
+        let errorResponse = Response.errorResponse(error);
+        res.status(errorResponse.code).json(errorResponse);
+    }
+});
+router.post('/add', async(req,res)=>{
+    let body =req.body;
+    try {
+
+        if (!body.name) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error!", "name field must be filled");
+
+        let category = new Categories({
+            name: body.name,
+            is_active: true,
+            created_by: req.user?.id
+        });
+
+        await category.save();
+       await category.save();
+       res.json(Response.successResponse({success:true}));
+
+    } catch (error) {
+        let errorResponse = Response.errorResponse(error);
+        res.status(errorResponse.code).json(errorResponse);
     }
 });
 
